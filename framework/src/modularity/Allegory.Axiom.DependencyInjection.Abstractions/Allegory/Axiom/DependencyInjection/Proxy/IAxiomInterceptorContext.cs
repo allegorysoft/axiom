b5884c93a3ca704ea2ee9a50358b47ff77aae334
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-02-26 02:42:11 +0300

    IAxiomInterceptorContext.cs set properties nullable

--- framework/src/modularity/Allegory.Axiom.DependencyInjection.Abstractions/Allegory/Axiom/DependencyInjection/Proxy/IAxiomInterceptorContext.cs
+++ framework/src/modularity/Allegory.Axiom.DependencyInjection.Abstractions/Allegory/Axiom/DependencyInjection/Proxy/IAxiomInterceptorContext.cs
@@ -6,8 +6,8 @@ namespace Allegory.Axiom.DependencyInjection.Proxy;
 public interface IAxiomInterceptorContext
 {
     MethodInfo Method { get; }
-    object[] Arguments { get; }
-    object ReturnValue { get; set; }
-    object Target { get; }
+    object?[] Arguments { get; }
+    object? ReturnValue { get; set; }
+    object? Target { get; }
     Task ProceedAsync();
 }
