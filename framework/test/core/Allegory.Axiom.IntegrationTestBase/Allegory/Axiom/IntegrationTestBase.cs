a8b422d95f99abca25a32c96fd83c071c1500007
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-04-05 17:36:23 +0300

    Integration test base, unit of work test projects created

---
+++ framework/test/core/Allegory.Axiom.IntegrationTestBase/Allegory/Axiom/IntegrationTestBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Allegory.Axiom.Hosting;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Allegory.Axiom;
+
+public abstract class IntegrationTestBase : IAsyncLifetime
+{
+    protected IHost Host { get; set; } = null!;
+    protected IServiceProvider ServiceProvider => Host.Services;
+
+    public virtual async ValueTask InitializeAsync()
+    {
+        var builder = Microsoft.Extensions.Hosting.Host.CreateApplicationBuilder();
+        await builder.ConfigureApplicationAsync();
+        Host = builder.Build();
+        await Host.InitializeApplicationAsync();
+    }
+
+    public virtual ValueTask DisposeAsync()
+    {
+        Host.Dispose();
+        return ValueTask.CompletedTask;
+    }
+}
---
+++ framework/test/unit-of-work/Allegory.Axiom.UnitOfWork.Tests/Allegory/Axiom/UnitOfWork/UnitOfWorkManagerTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Testing.Platform.Services;
+using Xunit;
+
+namespace Allegory.Axiom.UnitOfWork;
+
+public class UnitOfWorkManagerTests : IntegrationTestBase
+{
+    [Fact]
+    public async Task Test()
+    {
+        var manager = ServiceProvider.GetRequiredService<IUnitOfWorkManager>();
+    }
+}
