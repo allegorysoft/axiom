dae7447c6aab5ab0b426d8d1782d94ad04420a22
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-04-06 20:14:29 +0300

    UnitOfWorkManagerTests.cs added

--- framework/test/unit-of-work/Allegory.Axiom.UnitOfWork.Tests/Allegory/Axiom/UnitOfWork/UnitOfWorkManagerTests.cs
+++ framework/test/unit-of-work/Allegory.Axiom.UnitOfWork.Tests/Allegory/Axiom/UnitOfWork/UnitOfWorkManagerTests.cs
@@ -1,5 +1,5 @@
-﻿using System.Threading.Tasks;
-using Microsoft.Testing.Platform.Services;
+﻿using Microsoft.Testing.Platform.Services;
+using Shouldly;
 using Xunit;
 
 namespace Allegory.Axiom.UnitOfWork;
@@ -7,8 +7,53 @@ namespace Allegory.Axiom.UnitOfWork;
 public class UnitOfWorkManagerTests : IntegrationTestBase
 {
     [Fact]
-    public async Task Test()
+    public void ShouldCreateUnitOfWork()
     {
         var manager = ServiceProvider.GetRequiredService<IUnitOfWorkManager>();
+
+        using (var root = manager.Begin())
+        {
+            manager.Current.ShouldNotBeNull();
+            manager.Current.ShouldBe(root);
+        }
+
+        manager.Current.ShouldBeNull();
+    }
+
+    [Fact]
+    public void ShouldJoinParentUnitOfWorkWhenParentExists()
+    {
+        var manager = ServiceProvider.GetRequiredService<IUnitOfWorkManager>();
+
+        using (var root = manager.Begin())
+        {
+            manager.Current.ShouldBe(root);
+            manager.Current.ShouldBeOfType<UnitOfWork>();
+
+            using (var child = manager.Begin())
+            {
+                manager.Current.ShouldBe(child);
+                manager.Current.ShouldBeOfType<ChildUnitOfWork>();
+                manager.Current.Parent.ShouldBe(root);
+            }
+        }
+    }
+
+    [Fact]
+    public void ShouldRestoreParentAfterChildDisposed()
+    {
+        var manager = ServiceProvider.GetRequiredService<IUnitOfWorkManager>();
+
+        using (var root = manager.Begin())
+        {
+            manager.Current.ShouldBe(root);
+
+            using (var child = manager.Begin())
+            {
+                manager.Current.ShouldBe(child);
+            }
+
+            manager.Current.ShouldBe(root);
+        }
     }
 }
