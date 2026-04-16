de3dc1e3e120077e5d32f2e8a5bac44becaf6ba4
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-04-16 13:50:42 +0300

    Allegory.Axiom.FileProviders.props created

--- framework/src/core/Allegory.Axiom.FileProviders/Allegory/Axiom/FileProviders/FileProviderOptions.cs
+++ framework/src/core/Allegory.Axiom.FileProviders/Allegory/Axiom/FileProviders/FileProviderOptions.cs
@@ -11,19 +11,22 @@ public class FileProviderOptions
 
     public void AddEmbedded<T>(string? root = null)
     {
-        Providers.Add(new EmbeddedFileProvider(typeof(T).Assembly, root));
+        Providers.Add(root == null
+            ? new ManifestEmbeddedFileProvider(typeof(T).Assembly)
+            : new ManifestEmbeddedFileProvider(typeof(T).Assembly, root));
     }
 
     public void AddEmbedded(Assembly assembly, string? root = null)
     {
-        Providers.Add(new EmbeddedFileProvider(assembly, root));
+        Providers.Add(root == null
+            ? new ManifestEmbeddedFileProvider(assembly)
+            : new ManifestEmbeddedFileProvider(assembly, root));
     }
 
     public void AddPhysical(string root, ExclusionFilters? filters = null)
     {
-        Providers.Add(
-            filters.HasValue
-                ? new PhysicalFileProvider(root, filters.Value)
-                : new PhysicalFileProvider(root));
+        Providers.Add(filters.HasValue
+            ? new PhysicalFileProvider(root, filters.Value)
+            : new PhysicalFileProvider(root));
     }
 }
