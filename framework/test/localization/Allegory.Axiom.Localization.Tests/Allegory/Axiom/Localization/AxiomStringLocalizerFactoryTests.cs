2c546d537d99be92869566b95d70bf6cc39dc6fd
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-04-21 17:01:54 +0300

    AxiomStringLocalizerFactoryTests created

---
+++ framework/test/localization/Allegory.Axiom.Localization.Tests/Allegory/Axiom/Localization/AxiomStringLocalizerFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Allegory.Axiom.FileProviders;
+using Allegory.Axiom.Localization.Resources;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Localization;
+using Xunit;
+
+namespace Allegory.Axiom.Localization;
+
+public class AxiomStringLocalizerFactoryTests : HostedIntegrationTestBase
+{
+    protected IStringLocalizerFactory LocalizerFactory => Service<IStringLocalizerFactory>();
+
+    protected override ValueTask ConfigureAsync(IHostApplicationBuilder builder)
+    {
+        builder.Services.Configure<FileProviderOptions>(options =>
+        {
+            options.AddEmbedded<AxiomStringLocalizerFactoryTests>();
+        });
+
+        builder.Services.Configure<LocalizationOptions>(options =>
+        {
+            options.Resources.Add<AxiomLocalizationResource>(
+                "en",
+                "/Allegory/Axiom/Localization/Resources/Directory-1",
+                "/Allegory/Axiom/Localization/Resources/Directory-2");
+        });
+
+        return ValueTask.CompletedTask;
+    }
+
+    [Fact]
+    public async Task Test()
+    {
+        var f = LocalizerFactory.Create(typeof(AxiomLocalizationResource));
+    }
+}
---
+++ framework/test/localization/Allegory.Axiom.Localization.Tests/Allegory/Axiom/Localization/Resources/AxiomLocalizationResource.cs
@@ -0,0 +1,3 @@
+namespace Allegory.Axiom.Localization.Resources;
+
+public class AxiomLocalizationResource {}
