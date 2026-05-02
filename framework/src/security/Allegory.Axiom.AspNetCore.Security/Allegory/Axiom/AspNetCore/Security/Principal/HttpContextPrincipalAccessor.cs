39b0c6affcc0973b7c94f004757b150b2f6737fc
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-05-02 17:23:48 +0300

    Security projects created

---
+++ framework/src/security/Allegory.Axiom.AspNetCore.Security/Allegory/Axiom/AspNetCore/Security/Principal/HttpContextPrincipalAccessor.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using Allegory.Axiom.Security.Principal;
+using Microsoft.AspNetCore.Http;
+
+namespace Allegory.Axiom.AspNetCore.Security.Principal;
+
+public class HttpContextPrincipalAccessor(IHttpContextAccessor httpContextAccessor) : ClaimsPrincipalAccessor
+{
+    public override ClaimsPrincipal? Current => HttpContextAccessor.HttpContext?.User ?? base.Current;
+
+    protected virtual IHttpContextAccessor HttpContextAccessor { get; } = httpContextAccessor;
+}
---
+++ framework/src/security/Allegory.Axiom.Security/Allegory/Axiom/Security/Principal/ClaimsPrincipalAccessor.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+using Allegory.Axiom.DependencyInjection;
+
+namespace Allegory.Axiom.Security.Principal;
+
+[Dependency(Strategy = RegistrationStrategy.TryAdd)]
+public class ClaimsPrincipalAccessor : IPrincipalAccessor
+{
+    public virtual ClaimsPrincipal? Current => ClaimsPrincipal.Current;
+}
