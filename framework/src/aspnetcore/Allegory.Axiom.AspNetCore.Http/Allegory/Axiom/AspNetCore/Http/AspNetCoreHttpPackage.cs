6bc4aa036758fefc570a011fb1d7a879ad49d3be
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-05-02 16:14:12 +0300

    Allegory.Axiom.AspNetCore.Http.csproj package created

---
+++ framework/src/aspnetcore/Allegory.Axiom.AspNetCore.Http/Allegory/Axiom/AspNetCore/Http/AspNetCoreHttpPackage.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Allegory.Axiom.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Allegory.Axiom.AspNetCore.Http;
+
+internal sealed class AspNetCoreHttpPackage : IConfigureApplication
+{
+    public static Task ConfigureAsync(IHostApplicationBuilder builder)
+    {
+        builder.Services.AddHttpContextAccessor();
+
+        return Task.CompletedTask;
+    }
+}
