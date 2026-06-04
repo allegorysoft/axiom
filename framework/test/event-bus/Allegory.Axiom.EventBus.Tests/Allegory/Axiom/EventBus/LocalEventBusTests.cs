e38af01c62f0c5b6d23a8910aa27562477c4fa92
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-06-04 11:43:06 +0300

    LocalEventBusTests.cs implemented

--- framework/test/event-bus/Allegory.Axiom.EventBus.Tests/Allegory/Axiom/EventBus/LocalEventBusTests.cs
+++ framework/test/event-bus/Allegory.Axiom.EventBus.Tests/Allegory/Axiom/EventBus/LocalEventBusTests.cs
@@ -1,57 +1,112 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Allegory.Axiom.UnitOfWork;
+using Shouldly;
 using Xunit;
 
 namespace Allegory.Axiom.EventBus;
 
 public class LocalEventBusTests(IntegrationTestFixture fixture) : IClassFixture<IntegrationTestFixture>
 {
+    protected ILocalEventBus EventBus => fixture.Service<ILocalEventBus>();
+
     [Fact]
-    public async Task Test()
+    public async Task ShouldPublishEventToHandler()
     {
-        var eventBus = fixture.Service<ILocalEventBus>();
+        var handler = fixture.Service<TestEventHandler>();
 
-        eventBus.Subscribe("a", static o =>
-        {
-            return Task.CompletedTask;
-        });
+        handler.Received.ShouldNotContain(e => e.Value == 1);
 
-        await eventBus.PublishAsync(new OrderCreated(123));
-        await eventBus.PublishAsync(
-            "a",
-            new
-            {
-                a = "nice"
-            });
+        await EventBus.PublishAsync(new TestEvent(1), onUnitOfWorkComplete: false);
+
+        handler.Received.ShouldContain(e => e.Value == 1);
+    }
+
+    [Fact]
+    public async Task ShouldPublishEventToAllHandlers()
+    {
+        var handler1 = fixture.Service<TestEventHandler>();
+        var handler2 = fixture.Service<TestEventHandler2>();
+
+        handler1.Received.ShouldNotContain(e => e.Value == 2);
+        handler2.Received.ShouldNotContain(e => e.Value == 2);
+
+        await EventBus.PublishAsync(new TestEvent(2), onUnitOfWorkComplete: false);
+
+        handler1.Received.ShouldContain(e => e.Value == 2);
+        handler2.Received.ShouldContain(e => e.Value == 2);
+    }
+
+    [Fact]
+    public async Task ShouldNotThrowWhenNoHandlerRegistered()
+    {
+        await Should.NotThrowAsync(() =>
+            EventBus.PublishAsync(new UnhandledTestEvent(), onUnitOfWorkComplete: false));
+    }
+
+    [Fact]
+    public async Task ShouldDeferHandlerUntilUnitOfWorkCompletes()
+    {
+        var handler = fixture.Service<TestEventHandler>();
+        var uowManager = fixture.Service<IUnitOfWorkManager>();
+
+        await using var uow = uowManager.Begin();
+        await EventBus.PublishAsync(new TestEvent(3), onUnitOfWorkComplete: true);
+
+        handler.Received.ShouldNotContain(e => e.Value == 3);
+
+        await uow.CompleteAsync(TestContext.Current.CancellationToken);
+
+        handler.Received.ShouldContain(e => e.Value == 3);
+    }
+
+    [Fact]
+    public async Task ShouldInvokeHandlerImmediatelyWhenNoActiveUnitOfWork()
+    {
+        var handler = fixture.Service<TestEventHandler>();
+
+        await EventBus.PublishAsync(new TestEvent(4), onUnitOfWorkComplete: true);
+
+        handler.Received.ShouldContain(e => e.Value == 4);
+    }
+
+    [Fact]
+    public async Task ShouldInvokeHandlerImmediatelyWhenOnUnitOfWorkCompleteIsFalse()
+    {
+        var handler = fixture.Service<TestEventHandler>();
+        var uowManager = fixture.Service<IUnitOfWorkManager>();
+
+        await using var uow = uowManager.Begin();
+        await EventBus.PublishAsync(new TestEvent(5), onUnitOfWorkComplete: false);
+
+        handler.Received.ShouldContain(e => e.Value == 5);
+
+        await uow.CompleteAsync(TestContext.Current.CancellationToken);
     }
 }
 
-public record OrderCreated(int OrderId);
-public record SomeCreated(int OrderId);
+file record TestEvent(int Value);
 
-public class OrderCreatedHandler : ILocalEventHandler<OrderCreated>
+file record UnhandledTestEvent;
+
+file class TestEventHandler : ILocalEventHandler<TestEvent>
 {
-    public Task HandleAsync(OrderCreated payload)
+    public List<TestEvent> Received { get; } = [];
+
+    public Task HandleAsync(TestEvent payload)
     {
+        Received.Add(payload);
         return Task.CompletedTask;
     }
 }
 
-public class OrderCreatedHandler2 : ILocalEventHandler<OrderCreated>
+file class TestEventHandler2 : ILocalEventHandler<TestEvent>
 {
-    public Guid Id { get;  } = Guid.NewGuid();
-    public Task HandleAsync(OrderCreated payload)
-    {
-        return Task.CompletedTask;
-    }
-}
+    public List<TestEvent> Received { get; } = [];
 
-public class A {}
-
-public class AHanlder : ILocalEventHandler<A>
-{
-    public Task HandleAsync(A payload)
+    public Task HandleAsync(TestEvent payload)
     {
+        Received.Add(payload);
         return Task.CompletedTask;
     }
 }
