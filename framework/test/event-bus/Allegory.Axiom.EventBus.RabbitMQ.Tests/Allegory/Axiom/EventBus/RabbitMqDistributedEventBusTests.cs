4aea940384cee48862b262b822c88bde26c7eb84
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-06-12 13:32:24 +0300

    Test project created

---
+++ framework/test/event-bus/Allegory.Axiom.EventBus.RabbitMQ.Tests/Allegory/Axiom/EventBus/RabbitMqDistributedEventBusTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Allegory.Axiom.EventBus;
+
+public class RabbitMqDistributedEventBusTests(IntegrationTestFixture fixture) : IClassFixture<IntegrationTestFixture>
+{
+    protected IDistributedEventBus EventBus => fixture.Service<IDistributedEventBus>();
+
+    [Fact]
+    public async Task Test()
+    {
+        var order = new OrderCreated()
+        {
+            Number = "001"
+        };
+
+        await Parallel.ForAsync(0, 10_000, async (_,_)=>
+        {
+            await EventBus.PublishAsync(order, DistributedMessagePublishMode.Immediate);
+        });
+    }
+}
+
+public class OrderCreated
+{
+    public string Number { get; set; }
+}
