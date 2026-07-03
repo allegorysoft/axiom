bfa4b0c6ae40270599c1a3fa40842eddc2aca6ac
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-07-03 11:07:48 +0300

    Allegory.Axiom.ConsoleApp created in playground folder

---
+++ framework/test/playground/Allegory.Axiom.ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using Allegory.Axiom.Hosting;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+await builder.ConfigureApplicationAsync();
+var app = builder.Build();
+await app.InitializeApplicationAsync();
+await app.RunAsync();
