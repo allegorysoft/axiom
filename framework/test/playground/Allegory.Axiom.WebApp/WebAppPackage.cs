ad23e42cde002c848e5a936f8c8fcd9f9850f0fd
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-07-10 18:14:18 +0300

    OpenTelemetry added to playground project

--- framework/test/playground/Allegory.Axiom.WebApp/WebAppPackage.cs
+++ framework/test/playground/Allegory.Axiom.WebApp/WebAppPackage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Allegory.Axiom.AspNetCore;
 using Allegory.Axiom.Hosting;
@@ -6,16 +7,59 @@ using Allegory.Axiom.UnitOfWork;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 internal sealed class WebAppPackage : IConfigureApplication, IInitializeApplication
 {
     public static Task ConfigureAsync(IHostApplicationBuilder builder)
     {
-        // Configure open telemetry
-        // Use rabbitmq distributed event bus
         builder.Services.AddOpenApi();
+        ConfigureOpenTelemetry(builder);
 
         return Task.CompletedTask;
+    }
+
+    private static void ConfigureOpenTelemetry(IHostApplicationBuilder builder)
+    {
+        /*
+          podman run --rm -it \
+             -p 18888:18888 \
+             -p 18889:18889 \
+             -e DOTNET_DASHBOARD_UNSECURED_ALLOW_ANONYMOUS=true \
+             mcr.microsoft.com/dotnet/aspire-dashboard:latest
+         */
+
+        var exporterOptionsAction = static (OtlpExporterOptions o) =>
+        {
+            o.Endpoint = new Uri("http://localhost:18889");
+            o.Protocol = OtlpExportProtocol.Grpc;
+        };
+
+        var resource = ResourceBuilder.CreateDefault()
+            .AddService("WebApp", serviceVersion: "1.0.0");
+
+        builder.Services
+            .AddOpenTelemetry()
+            .WithTracing(tracing => tracing
+                .SetResourceBuilder(resource)
+                .AddSource("Allegory.Axiom.EventBus")
+                .AddSource("Allegory.Axiom.UnitOfWork")
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddOtlpExporter(exporterOptionsAction))
+            .WithMetrics(metrics => metrics
+                .SetResourceBuilder(resource)
+                //.AddMeter("TelemetryName")
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddOtlpExporter(exporterOptionsAction))
+            .WithLogging(logging => logging
+                .SetResourceBuilder(resource)
+                .AddOtlpExporter(exporterOptionsAction));
     }
 
     public static Task InitializeAsync(IHost host)
