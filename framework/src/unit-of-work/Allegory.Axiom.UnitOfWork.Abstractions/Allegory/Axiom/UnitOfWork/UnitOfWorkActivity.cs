63aab8ca4ffe3c03456aecbd175d1664151c035b
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-07-17 19:32:32 +0300

    Distributed event bus publish activity created

--- framework/src/unit-of-work/Allegory.Axiom.UnitOfWork.Abstractions/Allegory/Axiom/UnitOfWork/UnitOfWorkActivity.cs
+++ framework/src/unit-of-work/Allegory.Axiom.UnitOfWork.Abstractions/Allegory/Axiom/UnitOfWork/UnitOfWorkActivity.cs
@@ -1,6 +1,9 @@
+using System.Diagnostics;
+
 namespace Allegory.Axiom.UnitOfWork;
 
 public static class UnitOfWorkActivity
 {
     public const string Name = "Allegory.Axiom.UnitOfWork";
+    public static readonly ActivitySource Source = new(Name);
 }
