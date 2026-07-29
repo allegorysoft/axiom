0583ef582010a41bd1c5663518fa9a6c1b3ee1a2
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-07-29 13:23:15 +0300

    TenantContext optimzed for cache
    https://learn.microsoft.com/en-us/aspnet/core/performance/caching/hybrid?view=aspnetcore-10.0#optimize-performance

--- framework/src/multi-tenancy/Allegory.Axiom.MultiTenancy.Abstractions/Allegory/Axiom/MultiTenancy/TenantContext.cs
+++ framework/src/multi-tenancy/Allegory.Axiom.MultiTenancy.Abstractions/Allegory/Axiom/MultiTenancy/TenantContext.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Allegory.Axiom.Extensibility;
 
 namespace Allegory.Axiom.MultiTenancy;
 
-public class TenantContext(Guid id, string name, string normalizedName) : IReadOnlyExtraProperties
+[ImmutableObject(true)]
+public sealed class TenantContext(
+    Guid id,
+    string name,
+    string normalizedName,
+    IReadOnlyDictionary<string, object?>? extraProperties = null) 
+    : IReadOnlyExtraProperties
 {
     public Guid Id { get; } = id;
     public string Name { get; } = name;
     public string NormalizedName { get; } = normalizedName;
-    public IReadOnlyDictionary<string, object?> ExtraProperties { get; init; } = new Dictionary<string, object?>();
+    public IReadOnlyDictionary<string, object?> ExtraProperties { get; } = extraProperties ?? new Dictionary<string, object?>();
 }
