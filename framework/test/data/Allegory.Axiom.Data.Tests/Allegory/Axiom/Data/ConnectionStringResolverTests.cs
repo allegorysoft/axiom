42aea38edb4152eb6cc1bcb04a975b60ad8d5af4
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-08-05 18:18:55 +0300

    Data test project created

---
+++ framework/test/data/Allegory.Axiom.Data.Tests/Allegory/Axiom/Data/ConnectionStringResolverTests.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using Allegory.Axiom.MultiTenancy;
+using Xunit;
+
+namespace Allegory.Axiom.Data;
+
+public class ConnectionStringResolverTests(IntegrationTestFixture fixture) : IClassFixture<IntegrationTestFixture>
+{
+    private const string Tenant1 = "T-1", Tenant2 = "T-2";
+
+    protected IConnectionStringProvider ConnectionStringProvider => fixture.Service<IConnectionStringProvider>();
+    protected ITenantStore TenantStore => fixture.Service<ITenantStore>();
+    protected ITenantContextAccessor TenantContextAccessor => fixture.Service<ITenantContextAccessor>();
+
+    [Fact]
+    public async Task Test()
+    {
+        TenantContextAccessor.Set(await TenantStore.GetAsync(Tenant1));
+
+        var connection = await ConnectionStringProvider.GetAsync("");
+    }
+}
