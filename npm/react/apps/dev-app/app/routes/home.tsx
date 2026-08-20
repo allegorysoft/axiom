3c89f0d5afdac59ad0797914da63b5291c6d0dec
masum-ulu <masum.ulu@hotmail.com>
2026-08-20 16:22:21 +0300

    update home page

--- npm/react/apps/dev-app/app/routes/home.tsx
+++ npm/react/apps/dev-app/app/routes/home.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 import { useTranslation } from '@axiomframework/react-core';
+import { oAuthProvider, useOAuth } from '@axiomframework/react-oauth';
+import { Button } from '@axiomframework/react-theme/components';
 
 export default function Home() {
+  const navigate = useNavigate();
   const t = useTranslation('AxiomIdentity');
+  const auth = useOAuth((s) => s.token);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setIsAuthenticated(!!auth?.accessToken.length);
+  }, [auth?.accessToken]);
+
+  function logIn() {
+    oAuthProvider.get().redirectToLogin(() => navigate('/login'));
+  }
+
+  function logOut() {
+    oAuthProvider.get().logout();
+  }
+
   return (
     <>
-      <span>{t('Welcome', { name: 'Masum' })}</span>
+      <span className="mx-3">{t('Welcome', { name: 'Masum' })}</span>
+      {!isAuthenticated && <Button onClick={logIn}>{t('Login')}</Button>}
+      {isAuthenticated && <Button onClick={logOut}>{t('Logout')}</Button>}
     </>
   );
 }
