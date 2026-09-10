e8e76fbca997347788fee094f62a2af842a9a83a
masum-ulu <masum.ulu@hotmail.com>
2026-09-10 14:47:14 +0300

    wrap TooltipProvider

--- npm/react/apps/dev-app/app/app.tsx
+++ npm/react/apps/dev-app/app/app.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { RouterProvider } from 'react-router/dom';
+import { TooltipProvider } from '@axiomframework/react-theme/components';
 import { routes } from './routes';
 
 export const App = () => {
   return (
     <StrictMode>
-      <RouterProvider router={routes} />
+      <TooltipProvider>
+        <RouterProvider router={routes} />
+      </TooltipProvider>
     </StrictMode>
   );
 };
