6dcbfb06ee1c3d49def0c3f33cb5abda7aafb9c1
masum-ulu <masum.ulu@hotmail.com>
2026-09-12 14:14:03 +0300

    use theme-provider in app.tsx

--- npm/react/apps/dev-app/app/app.tsx
+++ npm/react/apps/dev-app/app/app.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react';
 import { RouterProvider } from 'react-router/dom';
+import { ThemeProvider } from '@axiomframework/react-core';
 import { TooltipProvider } from '@axiomframework/react-theme/components';
 import { routes } from './routes';
 
 export const App = () => {
   return (
     <StrictMode>
-      <TooltipProvider>
-        <RouterProvider router={routes} />
-      </TooltipProvider>
+      <ThemeProvider>
+        <TooltipProvider>
+          <RouterProvider router={routes} />
+        </TooltipProvider>
+      </ThemeProvider>
     </StrictMode>
   );
 };
--- npm/react/apps/dev-app/app/layout.tsx
+++ npm/react/apps/dev-app/app/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout() {
       <AppSidebar />
       <SidebarInset>
         <Header />
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-2">
           <Outlet />
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-muted/50" />
