553f4735520caed1cee692686b36c5615b3a4e0b
Seda Ulu <sedaseenn@gmail.com>
2026-09-16 15:20:43 +0300

    add: stick behavior

--- npm/react/libs/theme/src/components/header.tsx
+++ npm/react/libs/theme/src/components/header.tsx
@@ -20,7 +20,7 @@ export function Header() {
   const label = useDecodedHash();
 
   return (
-    <header className="flex h-14 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 px-3 md:px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 border mx-2 my-2 rounded-lg">
+    <header className="sticky top-2 z-50 flex h-14 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 px-3 md:px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 border mx-2 my-2 rounded-lg">
       <SidebarTrigger className="-ml-1" />
       <Separator
         orientation="vertical"
