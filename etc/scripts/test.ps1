09916cf2ea3f24344873090a4de5ec2a86fe3e1a
ahmetfarukulu <ahmetfarukulu@gmail.com>
2026-09-22 15:07:19 +0300

    Add code coverage support and reporting to CI workflow

--- etc/scripts/test.ps1
+++ etc/scripts/test.ps1
@@ -1,12 +1,36 @@
 param(
     [Alias("c")]
-    [string]$configuration = "Debug"
+    [string]$configuration = "Debug",
+    [switch]$Coverage,
+    [string]$CoverageOutput
 )
 
 . "$PSScriptRoot/base.ps1"
 
-foreach ($solution in $solutions) {    
-    dotnet test $solution --no-build -c $configuration
+if (-not $CoverageOutput) {
+    $CoverageOutput = Join-Path $root "TestResults/coverage.cobertura.xml"
+}
+
+foreach ($solution in $solutions) {
+
+    $testArgs = @(
+        "test",
+        "--solution", $solution,
+        "--no-build",
+        "-c", $configuration,
+        "--report-gh"
+    )
+
+    if ($Coverage) {
+        $testArgs += @(
+            "--coverage",
+            "--coverage-output-format", "cobertura",
+            "--coverage-output", $CoverageOutput
+        )
+    }
+
+    dotnet @testArgs
+
     if (-Not $?) {
         Write-Host ("Test failed for the solution: " + $solution)
         exit $LASTEXITCODE
