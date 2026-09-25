a9642c84301eab6a99f6c4e6759630848491138a
Seda Ulu <sedaseenn@gmail.com>
2026-09-25 15:33:50 +0300

    refactor: Enhance PasswordInput component with improved icon usage and optional leading icon

--- npm/react/libs/theme/src/components/password-input.tsx
+++ npm/react/libs/theme/src/components/password-input.tsx
@@ -1,30 +1,34 @@
 import * as React from 'react';
 import { HugeiconsIcon } from '@hugeicons/react';
-import { EyeIcon, EyeOffIcon, LockIcon } from '@hugeicons/core-free-icons';
+import { ViewIcon, EyeOffIcon, KeyRoundIcon } from '@hugeicons/core-free-icons';
 import { InputGroup, InputGroupAddon, InputGroupInput } from './ui/input-group';
 
+type Props = {showLeadingIcon?: boolean;};
 export function PasswordInput({
+  showLeadingIcon = true,
   ...inputProps
-}: React.ComponentProps<typeof InputGroupInput>) {
+}: React.ComponentProps<typeof InputGroupInput> & Props) {
   const [visible, setVisible] = React.useState(false);
 
   return (
-    <InputGroup>
-      <InputGroupAddon>
-        <HugeiconsIcon icon={LockIcon} strokeWidth={2} />
-      </InputGroupAddon>
+    <InputGroup className="password-input">
+      {showLeadingIcon && (
+        <InputGroupAddon>
+          <HugeiconsIcon icon={KeyRoundIcon} strokeWidth={2} />
+        </InputGroupAddon>
+      )}
 
       <InputGroupInput {...inputProps} type={visible ? 'text' : 'password'} />
 
-      <InputGroupAddon align="inline-end">
+      <InputGroupAddon align="inline-end" className="pr-2.5 has-[>button]:mr-0">
         <button
           id="togglePassword"
           type="button"
           onClick={() => setVisible((v) => !v)}
           aria-label={visible ? 'Hide password' : 'Show password'}
-          tabIndex={-1}
+          aria-pressed={visible}
         >
-          {visible ? <HugeiconsIcon icon={EyeOffIcon} strokeWidth={2} /> : <HugeiconsIcon icon={EyeIcon} strokeWidth={2} />}
+          {visible ? <HugeiconsIcon icon={EyeOffIcon} strokeWidth={2} className="size-4" /> : <HugeiconsIcon icon={ViewIcon} strokeWidth={2} className="size-4" />}
         </button>
       </InputGroupAddon>
     </InputGroup>
